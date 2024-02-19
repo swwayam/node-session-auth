0895820a1849d5c2541e9044a53c6d4394ab069c
swayam suryavanshi <codingswayam24@gmail.com>
2024-02-19 17:09:36 +0530

    Starter Code Ready

--- app.js
+++ app.js
@@ -1,84 +1,58 @@
+// Necessary Imports for Express App
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const { registerUser, authenticateUser } = require("./database");
 
 const app = express();
-const PORT = 3000;
+const PORT = 1337;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(
-  session({
-    secret: "your_secret_key",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60000 }, // 1 minute
-  })
-);
+// ******************************************
 
-// Middleware to check if user is authenticated
+// Modify Code Below This Line
+
+
+// ******************************************
+
+// Space For - Using the bodyParser middleware
+
+
+// Space For - Using the express-session middleware (this is for session management)
+
+
+// Space For - Writing the `isAuthenticated` middleware
 function isAuthenticated(req, res, next) {
-  if (!req.session.userId) {
-    return res.send("You must login to view this page.");
-  }
-  next();
+  
 }
 
-// Middleware to extend session expiry
+// Space For - Writing the `extendSessionExpiry` middleware
 function extendSessionExpiry(req, res, next) {
-  if (req.session) {
-    // Reset the expiration time after each request
-    req.session._garbage = Date();
-    req.session.touch();
-  }
-  next();
+ 
 }
 
-// Apply extendSessionExpiry middleware globally
+// Applying `extendSessionExpiry` middleware globally
 app.use(extendSessionExpiry);
 
+
+// Space For - Writing `/register` route
 app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    console.log(req.body);
-    // Validate input
-    if (!username || !password) {
-      return res.status(400).send('Username and password are required');
-    }
-  
-    registerUser(username, password, (err) => {
-      if (err) {
-        // Handle unique constraint violation for username
-        if (err.code === 'SQLITE_CONSTRAINT') {
-          return res.status(409).send('This username is already taken.'); // 409 Conflict
-        } else {
-          console.error(err);
-          return res.status(500).send('Failed to register user due to an unexpected error.'); // 500 Internal Server Error
-        }
-      }
-      res.send('User registered successfully.');
-    });
-  });
-  
+   
 
-// Login endpoint remains the same
-
-// Protected home route
-app.get("/home", isAuthenticated, (req, res) => {
-  res.send("Welcome to the protected home page!");
 });
-// Login endpoint
+  
+
+// Space For - Writing `/login` route
 app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  authenticateUser(username, password, (err, user) => {
-    if (err || !user) {
-      return res.send("Login failed.");
-    }
-    req.session.userId = user.id;
-    res.send("Logged in successfully.");
-  });
+  
 });
 
+// Space For - Writing `/home` route (Protected Route)
+app.get("/home", (req, res) => {
+  
+});
+
+
+// Do not modify any code below this line
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- database.js
+++ database.js
@@ -1,56 +1,25 @@
+// Necessary Imports 
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 
-// Open a database
-let db = new sqlite3.Database('./users.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the users database.');
-});
+// Write Code For - new sqlite3.Database
 
-db.run(`CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
-)`);
 
+
+// Write Code For - Creating the table
+
+
+// Write Code For - Register user
 function registerUser(username, password, callback) {
-  bcrypt.hash(password, 10, (err, hash) => {
-    if (err) {
-      return callback(err);
-    }
-    const sql = `INSERT INTO users (username, password) VALUES (?, ?)`;
-    db.run(sql, [username, hash], function(err) {
-      if (err) {
-        return callback(err);
-      }
-      callback(null);
-    });
-  });
+  
 }
 
+// Write Code For - Authenticate user (to check if user is present in DB and to check for the correct password)
 function authenticateUser(username, password, callback) {
-  const sql = `SELECT * FROM users WHERE username = ?`;
-  db.get(sql, [username], (err, row) => {
-    if (err) {
-      return callback(err, null);
-    }
-    if (row) {
-      bcrypt.compare(password, row.password, (err, result) => {
-        if (err) {
-          return callback(err, null);
-        }
-        if (result) {
-          return callback(null, row);
-        } else {
-          return callback(null, false);
-        }
-      });
-    } else {
-      return callback(null, false);
-    }
-  });
+  
+
 }
 
+
+// Do not modify any code below this line
 module.exports = { registerUser, authenticateUser };
